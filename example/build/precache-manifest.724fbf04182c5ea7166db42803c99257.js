53a2dcf4c465d9311942a52afee8c3f179be5284
Lukas Zirngibl <lukas.e.zirngibl@gmail.com>
2020-08-23 10:26:31 +0200

    add build

---
+++ example/build/precache-manifest.724fbf04182c5ea7166db42803c99257.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec705eadf680f0629905ce6d173ff95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a453037b968a013c59",
+    "url": "/static/js/2.ee40f5c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ee40f5c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fce94c3ee22092e9921",
+    "url": "/static/js/main.33918b93.chunk.js"
+  },
+  {
+    "revision": "c5a5a7d1eefcf4cd8044",
+    "url": "/static/js/runtime-main.90bb9db5.js"
+  }
+]);
